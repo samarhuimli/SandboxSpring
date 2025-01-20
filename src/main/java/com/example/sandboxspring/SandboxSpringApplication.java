e99cca37f8b01ce1dd308e092bd134d3ffab976d
samarhuimli <huimlisamar@gmail.com>
2025-01-20 10:41:12 +0100

    first commit

---
+++ src/main/java/com/example/sandboxspring/SandboxSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandboxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandboxspring/SandboxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandboxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
